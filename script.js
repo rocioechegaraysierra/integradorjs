d5b8a12894a2df01220816267f94cb6993940b39
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2022-10-28 12:09:07 -0300

    Se remueve mensaje anterior si calcula de nuevo el resumen

--- script.js
+++ script.js
@@ -9,27 +9,32 @@ botonResumen.addEventListener("click",
         let costo = document.querySelector("#Costo").value
         let categoria = document.querySelector("#Categoria").value
         let descuento = descuentoRealizado(categoria, costo)
-       
+
         let contenedorNombre = document.querySelector("#Mensaje")
-        let mensajeNombreCompleto = document.createElement("p")
-        mensajeNombreCompleto.textContent = "Descuento a nombre de: " + nombre + " " + apellido + ". Será enviado al correo: " + correo + ". El costo de su viaje sin descuento: " + costo + ". Total a pagar es: $" + descuento
-        contenedorNombre.appendChild(mensajeNombreCompleto);
-        
+
+        while (contenedorNombre.hasChildNodes()) {
+            contenedorNombre.removeChild(contenedorNombre.firstChild);
+        }
+
+        let mensaje = document.createTextNode("p")
+        mensaje.textContent = "Descuento a nombre de: " + nombre + " " + apellido + ". Será enviado al correo: " + correo + ". El costo de su viaje sin descuento: " + costo + ". Total a pagar es: $" + descuento
+        contenedorNombre.appendChild(mensaje);
+
     })
 
 function descuentoRealizado(categoria, costo) {
     let descuentoParcial;
-    if (categoria == "Menores"){
-        let porcentajeMenores = 0.80*costo
-        descuentoParcial = costo-porcentajeMenores;
+    if (categoria == "Menores") {
+        let porcentajeMenores = 0.80 * costo
+        descuentoParcial = costo - porcentajeMenores;
         return descuentoParcial;
-    }if (categoria == "Jubilados"){
-        let porcentajeJubilados= 0.50*costo
-        descuentoParcial = costo-porcentajeJubilados;
+    } if (categoria == "Jubilados") {
+        let porcentajeJubilados = 0.50 * costo
+        descuentoParcial = costo - porcentajeJubilados;
         return descuentoParcial;
-    }if(categoria == "Estudiantes"){
-        let porcentajeEstudiantes= 0.15*costo
-        descuentoParcial = costo-porcentajeEstudiantes;
+    } if (categoria == "Estudiantes") {
+        let porcentajeEstudiantes = 0.15 * costo
+        descuentoParcial = costo - porcentajeEstudiantes;
         return descuentoParcial;
     }
 }   
